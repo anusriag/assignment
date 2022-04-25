2523138877583b41938009f89dcfbf835c381f11
anusriag <102174251+anusriag@users.noreply.github.com>
2022-04-25 14:10:52 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+	console.log(a + b);
+}
+function sub(a, b) {
+	console.log(a - b);
+}
+function multiply(a, b) {
+	console.log(a * b);
+}
+function divide(a, b) {
+	console.log(a / b);
+}
+
+add(4,5)
+sub(4,5)
+multiply(4,5)
+divide(4,5)
